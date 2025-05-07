f7a72e028f187fa34e0fe168e877983081353a13
Grzegorz Listwan <listwan94@gmail.com>
2025-05-07 11:26:56 +0200

    Created BottomNavGuest component

--- mobile-app/LSOgo/screens/CalendarScreen.js
+++ mobile-app/LSOgo/screens/CalendarScreen.js
@@ -12,6 +12,7 @@ import {
   SafeAreaView,
 } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
+import BottomNavGuest from "../components/BottomNavGuest";
 
 const { width, height } = Dimensions.get("window");
 export default function CalendarScreen({ route }) {
@@ -81,38 +82,7 @@ export default function CalendarScreen({ route }) {
           ))}
         </ScrollView>
       </SafeAreaView>
-      {!loggedIn && (
-        <View style={styles.bottomNav}>
-          <TouchableOpacity style={styles.navItem}>
-            <Image
-              source={require("../assets/home.png")}
-              style={styles.navIcon}
-            />
-            <Text style={styles.navText}>Home</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.navItem}>
-            <Image
-              source={require("../assets/about.png")}
-              style={styles.navIcon}
-            />
-            <Text style={styles.navText}>O nas</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.navItem}>
-            <Image
-              source={require("../assets/contact.png")}
-              style={styles.navIcon}
-            />
-            <Text style={styles.navText}>Kontakt</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.navItem}>
-            <Image
-              source={require("../assets/login.png")}
-              style={styles.navIcon}
-            />
-            <Text style={styles.navText}>Log in</Text>
-          </TouchableOpacity>
-        </View>
-      )}
+      {!loggedIn && <BottomNavGuest />}
     </ImageBackground>
   );
 }
@@ -219,32 +189,5 @@ const styles = StyleSheet.create({
     fontFamily: "Cinzel-Regular",
     fontSize: RFValue(14),
     color: "#1a1204",
-  },
-  bottomNav: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    paddingTop: RFValue(10),
-    paddingLeft: RFValue(10),
-    paddingRight: RFValue(10),
-    paddingBottom: RFValue(15),
-    borderColor: "#1a1204",
-    backgroundColor: "#FDE09F",
-  },
-  navItem: {
-    alignItems: "center",
-  },
-  navText: {
-    fontSize: RFValue(12),
-    color: "#1a1204",
-  },
-  navIcon: {
-    width: RFValue(15),
-    height: RFValue(15),
-    resizeMode: "contain",
   },
 });
